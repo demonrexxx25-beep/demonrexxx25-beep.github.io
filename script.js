91548ee6ed8be5dbf8647b41c01f17b4c02dd73e
SNsentierz.github.io <demonrexxx25@gmail.com>
2026-02-07 07:16:30 -0500

    Add showTab function to manage tab visibility

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function showTab(tabId) {
+  document.querySelectorAll('.tab').forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  document.getElementById(tabId).classList.add('active');
+}
 
